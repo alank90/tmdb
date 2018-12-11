d8bcde1e4e237e43387f5dc8df88623bb5308edc
akillian@outlook.com <akillian@outlook.com>
2018-12-11 18:31:53 -0500

    This is working call to TMDB API

--- src/js/helper-functions/tmdbQuery.js
+++ src/js/helper-functions/tmdbQuery.js
@@ -4,67 +4,70 @@
 // ======= Let's Get Movie Info w/async/await =========== //
 // ====================================================== //
 /* jshint ignore:start */
-const getMovieInfo = async function() {
-  //  Begin the two AJAX calls needed to first retrieve Movie ID
-  //  and then retrieve Movie Info
-  var sUrl,
-    oData,
-    iTmdbId = 0;
-  let sTmdbQuery = $("form").serialize();
-  let api_key = "5888233c985dfa60ed6be20d8e6726a1";
 
-  // Query string to retrieve TMDB movie id
-  let sQueryMovieIdUrl =
-    "https://api.themoviedb.org/3/search/movie?api_key=5888233c985dfa60ed6be20d8e6726a1&" +
-    sTmdbQuery;
+const tmdbQuery = async function() {
+  try {
+    //  Begin the two AJAX calls needed to first retrieve Movie ID
+    //  and then retrieve Movie Info
+    var sUrl,
+      oData,
+      iTmdbId = 0;
+    let sTmdbQuery = $("form").serialize();
+    let api_key = "5888233c985dfa60ed6be20d8e6726a1";
 
-  // Ajax Query settings for movie id
-  let settingsAjax1 = {
-    dataType: "json", 
-    async: true,
-    crossDomain: true,
-    url: sQueryMovieIdUrl,
-    method: "GET",
-    headers: {},
-    data: "{}"
-  };
+    // Query string to retrieve TMDB movie id
+    let sQueryMovieIdUrl =
+      "https://api.themoviedb.org/3/search/movie?api_key=5888233c985dfa60ed6be20d8e6726a1&" +
+      sTmdbQuery;
 
-  // get the movie id first from TMDB database
-  let idResponse = await $.ajax(settingsAjax1);
-
-  // Check if any results returned
-  if (idResponse.total_results > 0) {
-    iTmdbId = idResponse.results[0].id;
-
-    //Query string to retrieve Movie Info
-    let sQueryMovieInfoUrl =
-      "https://api.themoviedb.org/3/movie/" + iTmdbId + "?api_key=" + api_key;
-
-    let settingsAjax2 = {
+    // Ajax Query settings for movie id
+    let settingsAjax1 = {
       dataType: "json",
       async: true,
       crossDomain: true,
-      url: sQueryMovieInfoUrl,
+      url: sQueryMovieIdUrl,
       method: "GET",
       headers: {},
       data: "{}"
     };
 
-    // Get movie info via the movie id with second AJAX call to the TMDB database
-    let movieInfo = await $.ajax(settingsAjax2);
+    // get the movie id first from TMDB database
+    let idResponse = await $.ajax(settingsAjax1);
+    console.log(idResponse);
 
-    return movieInfo;
-  } else {
-    $oContainer.addClass("hidden");
-    $oError.removeClass("hidden");
-    $oError.html(
-      "<p class='title'>No Movie Found. Try Alternate Spelling or check the Release Date if you entered one...</p>"
-    );
-  } // end if/else
+    // Check if any results returned
+    if (idResponse.total_results > 0) {
+      iTmdbId = idResponse.results[0].id;
+
+      //Query string to retrieve Movie Info
+      let sQueryMovieInfoUrl =
+        "https://api.themoviedb.org/3/movie/" + iTmdbId + "?api_key=" + api_key;
+
+      let settingsAjax2 = {
+        dataType: "json",
+        async: true,
+        crossDomain: true,
+        url: sQueryMovieInfoUrl,
+        method: "GET",
+        headers: {},
+        data: "{}"
+      };
+
+      // Get movie info via the movie id with second AJAX call to the TMDB database
+      let movieInfo = await $.ajax(settingsAjax2);
+      console.log(movieInfo);
+      return movieInfo;
+    } else {
+      $oContainer.addClass("hidden");
+      $oError.removeClass("hidden");
+      $oError.html(
+        "<p class='title'>No Movie Found. Try Alternate Spelling or check the Release Date if you entered one...</p>"
+      );
+    } // end if/else
+  } catch (e) {
+    console.log(e);
+    $("#poster").html("<h3>Error retrieving movie info</h3>");
+  }
 };
-/* jshint ignore:end */
-// =========================================================== //
-// ========= end tmdbQuery function ========================== //
-// =========================================================== //
 
-module.exports.movieInfo = getMovieInfo();
+module.exports = tmdbQuery;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 // =========  /src/js/main.js ============================ //
 
 // ========== Module Dependencies ================= //
-const displayMoviePage = require("./helper-functions/displayMoviePage");
+const tmdbQuery = require("./helper-functions/tmdbQuery");
 
 $(document).ready(function() {
   // =====  Declare Method Variables ============= //
@@ -10,12 +10,13 @@ $(document).ready(function() {
   var $oPoster = $("#poster");
   var $oClear = $("#clear");
   var $oError = $(".error_message");
+
   // ===== End Variable Declarations =========== //
 
   $oContainer.addClass("hidden");
   $oClear.addClass("hidden");
   $oError.addClass("hidden");
-  
+
   // ===================================================================== //
   // ============ TMDB Query EventHandler ================================ //
   // ===================================================================== //
@@ -25,12 +26,81 @@ $(document).ready(function() {
       '<center><img src="./src/img/loading.gif" alt="loading..."></center>'
     ); //gif while poster loads.
 
+    /* jshint ignore:start */
+    let displayMoviePage = async function() {
+      let oMovieInfo = await tmdbQuery();
+
+      // Now we can begin output of oMovieInfo to the page
+      // First Lets check state of the Movie Info .container
+      if ($oError.not(".hidden")) {
+        $oError.addClass("hidden");
+      }
+
+      // ========= Let's retrieve the Movie poster image ============= //
+      let sMoviePoster =
+        "https://image.tmdb.org/t/p/w342/" + oMovieInfo.poster_path;
+      $("#poster").html(
+        "<img src='" + sMoviePoster + "' alt='No Poster Available'>"
+      );
+      if ($("#poster").hasClass("hidden")) {
+        $("#poster").removeClass("hidden");
+      }
+      // ========== end retrieve Movie Poster Image ================ //
+
+      // Let's fill in the page with oMovieInfo object retrieved from TMDB
+      $oContainer
+        .find(".title")
+        .html("<p class='title'>Movie Title:</p>" + oMovieInfo.title);
+      $oContainer
+        .find(".tagline")
+        .html("<p class='tagline'></p>" + oMovieInfo.tagline);
+      $oContainer
+        .find(".plot")
+        .html("<p class='plot'>Movie Overview</p>" + oMovieInfo.overview);
+      $oContainer
+        .find(".release_date")
+        .html(
+          "<p class='release_date'>Release Date:</p>" + oMovieInfo.release_date
+        );
+      oMovieInfo.revenue = oMovieInfo.revenue
+        .toFixed(2)
+        .replace(/(\d)(?=(\d{3})+\.)/g, "$1,"); //Convert to Dollars
+      $oContainer
+        .find(".revenue")
+        .html(
+          "<p class='revenue'>Movie Revenues:</p>" + "$" + oMovieInfo.revenue
+        );
+      $oContainer
+        .find(".runtime")
+        .html(
+          "<p class='runtime'>Runtime:</p>" + oMovieInfo.runtime + " Minutes"
+        );
+      // Check if there is a Movie Page URL
+      if (oMovieInfo.homepage) {
+        $oContainer.find(".movie_url").attr({
+          href: oMovieInfo.homepage,
+          target: "_blank"
+        });
+      } else {
+        $oContainer
+          .find("p .movie_url ")
+          .text("Movie Page Not Available")
+          .attr("href", "");
+      }
+      $oContainer.removeClass("hidden"); // Make Results Container Visible
+      $oClear.removeClass("hidden"); // Show the clear button
+    };
+    /* jshint ignore:end */
+    // =========================================================================== //
+    // ============== End of displayMoviePage function =========================== //
+    // =========================================================================== //
+
     displayMoviePage();
+
+    // ===================================================================== //
+    // ============ End TMDB Query EventHandler ============================ //
+    // ===================================================================== //
   });
-  // ===================================================================== //
-  // ============ End TMDB Query EventHandler ============================ //
-  // ===================================================================== //
-  
 
   // Reset Form Function ==================================================
   function resetForm($form) {
