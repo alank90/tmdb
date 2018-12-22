69c59906088243dc66caec91f38dabfc985d5951
akillian@outlook.com <akillian@outlook.com>
2018-12-21 21:13:11 -0500

    commit

--- bio-addon.js
+++ bio-addon.js
@@ -1,16 +1,34 @@
-/* let $oBiography = $(el).children(".bio");
-         */
+let $oBiography = $(el).closest("li");
+
         // Check if p.bio is in the DOM or not
-        if ($(".production > .bio").length) {
-            // need to remove p.bio from DOM
-            $(".bio").toggleClass("hidden");
-          } else {
-            $(".production").append(
-              "<p class='bio'>" +
-                oPersonInfo.biography +
-                "<br>" +
-                "Born: " +
-                oPersonInfo.place_of_birth +
-                "</p>"
-            );
-          }
+        if ($oBiography.children(".bio").length === 0) {
+          // need to add p.bio to DOM
+          $oBiography.append(
+            "<p class='bio' data-crew-index=" +
+              $(el).attr("data-crew-index") +
+              ">" +
+              oPersonInfo.biography +
+              "<br>" +
+              "Born: " +
+              oPersonInfo.place_of_birth +
+              "</p>"
+          );
+          // Newly clicked person has not been clicked before so
+          // add it to the DOM
+        } else if (
+          $(el).attr("data-crew-index") !==
+          $oBiography.children(".bio").attr("data-crew-index")
+        ) {
+          $oBiography.append(
+            "<p class='bio' data-crew-index=" +
+              $(el).attr("data-crew-index") +
+              ">" +
+              oPersonInfo.biography +
+              "<br>" +
+              "Born: " +
+              oPersonInfo.place_of_birth +
+              "</p>"
+          );
+        } else {
+          $oBiography.children(".bio").toggleClass("hidden");
+        }
